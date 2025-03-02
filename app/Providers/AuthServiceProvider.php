e1e3b8383fb700cefa6a4637939679afcc703c75
devnouveau <chowbiz715@gmail.com>
2025-03-03 08:02:20 +0900

    feat : Oauth2 암묵적 승인 방식 사용 활성화

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,6 +30,8 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
         Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
-        //
+
+        Passport::enableImplicitGrant();
+
     }
 }
